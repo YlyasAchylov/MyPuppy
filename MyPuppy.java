9b40822031a011f558ebd53db523214386627a50
Ylyas Achylov <30547983+YlyasAchylov@users.noreply.github.com>
2017-08-02 00:32:04 +0300

    Update MyPuppy.java

--- MyPuppy.java
+++ MyPuppy.java
@@ -1,13 +1,26 @@
 
 public class MyPuppy {
-
+int puppyAge;
 	public MyPuppy(String name) {
 		System.out.println("Passed name is: " + name);
 	}
 
-	public static void main(String[] args) {
+	
+public static void setAge(int age){
+	puppyAge = age;
+}
+	public int getAge() {
+	System.out.println("Puppys age is: " + puppyAge);
+		return puppyAge;
+	}
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyPuppy puppy = new MyPuppy("Bob");
-	}
+        puppy.setAge(2);
+	puppy.getAge();
+        System.out.println("Variable Value :" + puppy.puppyAge);
+
 
 }
+
+       }
