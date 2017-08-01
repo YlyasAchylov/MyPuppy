5af6033be77baafaecc039b3ce79fdbc12cbc4d3
Ylyas Achylov <30547983+YlyasAchylov@users.noreply.github.com>
2017-08-02 00:08:11 +0300

    Add files via upload

---
+++ MyPuppy.java
@@ -0,0 +1,13 @@
+
+public class MyPuppy {
+
+	public MyPuppy(String name) {
+		System.out.println("Passed name is: " + name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyPuppy puppy = new MyPuppy("Bob");
+	}
+
+}
